4658f6acd98b8442ae3f3e0ba2c232dde64f1871
h4hn <hahnischiltach@gmail.com>
2023-11-02 14:26:50 +0100

    [SCRUM-28] - Basic Spielregeln implementieren
    feat:
    - Überprüfung von zu gehendem Pfad der Figur auf dort stehende Figuren

--- src/main/java/chessosaurus/review/BishopMoveReviewer.java
+++ src/main/java/chessosaurus/review/BishopMoveReviewer.java
@@ -10,6 +10,7 @@ public class BishopMoveReviewer extends MoveReviewerBase{
     boolean isSpecificLegalMove(Move move, Board chessboard) {
         Square from = move.getFrom();
         Square to = move.getTo();
+        Square[][] board = chessboard.getChessboard();
         Color color = from.getPiece().getColor();
         int fromFile = from.getFile();
         int toFile = to.getFile();
@@ -19,11 +20,39 @@ public class BishopMoveReviewer extends MoveReviewerBase{
         for(int i = 1; i<=8; i++){
             if(toFile == fromFile+i || toFile == fromFile-i){
                 if(toRank == toRank+i || toRank == fromRank-i){
+                    if(toRank > fromRank){
+                        if(toFile > fromFile){
+                            for(int j = 1; j < toRank-fromRank; j++){
+                                if(board[fromRank+j][fromFile+j].getPiece() != null){
+                                    return false;
+                                }
+                            }
+                        }else {
+                            for(int j = 1; j < toRank-fromRank; j++){
+                                if(board[fromRank+j][fromFile-j].getPiece() != null){
+                                    return false;
+                                }
+                            }
+                        }
+                    } else {
+                        if(toFile > fromFile){
+                            for(int j = fromRank; j < fromRank-toRank; j--){
+                                if(board[fromRank-j][fromFile+j].getPiece() != null){
+                                    return false;
+                                }
+                            }
+                        } else {
+                            for(int j = fromRank; j < fromRank-toRank; j--){
+                                if(board[fromRank-j][fromFile-j].getPiece() != null){
+                                    return false;
+                                }
+                            }
+                        }
+                    }
                     return true;
                 }
             }
         }
-
         return false;
     }
 }
--- src/main/java/chessosaurus/review/RookMoveReviewer.java
+++ src/main/java/chessosaurus/review/RookMoveReviewer.java
@@ -10,16 +10,43 @@ public class RookMoveReviewer extends MoveReviewerBase{
     boolean isSpecificLegalMove(Move move, Board chessboard) {
         Square from = move.getFrom();
         Square to = move.getTo();
+        Square[][] board = chessboard.getChessboard();
         Color color = from.getPiece().getColor();
         int fromFile = from.getFile();
         int toFile = to.getFile();
         char fromRank = from.getRank();
         char toRank = to.getRank();
 
-        if(toFile == fromFile && toRank != fromRank){
 
+        if(toFile == fromFile && toRank != fromRank){
+            if(toRank > fromRank){
+                for(int i = 1; i < toRank-fromRank; i++){
+                    if(board[fromRank+i][fromFile].getPiece() != null){
+                        return false;
+                    }
+                }
+            } else {
+                for(int i = fromRank; i < fromRank-toRank; i--){
+                    if(board[fromRank-i][fromFile].getPiece() != null){
+                        return false;
+                    }
+                }
+            }
             return true;
         } else if (toRank == fromRank && toFile != fromFile) {
+            if(toFile > fromFile){
+                for(int i = 1; i < toFile-fromFile; i++){
+                    if(board[fromRank][fromFile+i].getPiece() != null){
+                        return false;
+                    }
+                }
+            } else {
+                for(int i = fromFile; i < fromFile-toFile; i--){
+                    if(board[fromRank][fromFile-i].getPiece() != null){
+                        return false;
+                    }
+                }
+            }
             return true;
         } else return false;
     }
