7c73d97da05e74dc2d9c654cf69ed45593fcd321
h4hn <hahnischiltach@gmail.com>
2023-11-02 17:06:15 +0100

    [SCRUM-28] - Basic Spielregeln implementieren
    feat:
    - Implementierung des ReviewerContext

--- src/main/java/chessosaurus/review/ReviewerContext.java
+++ src/main/java/chessosaurus/review/ReviewerContext.java
@@ -1,4 +1,18 @@
 package chessosaurus.review;
 
+import chessosaurus.base.Board;
+import chessosaurus.base.Move;
+import chessosaurus.base.Piece;
+
 public class ReviewerContext {
+
+    private IMoveReviewer reviewer;
+
+    public void setReviewer(Piece piece){
+
+    }
+    public boolean isLegalMove(Move move, Board board){
+
+        return reviewer.isLegalMove(move, board);
+    }
 }
