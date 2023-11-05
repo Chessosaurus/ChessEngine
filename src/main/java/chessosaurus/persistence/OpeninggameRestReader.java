bbc4cb9e8505cb6eba993ada5abe29dd8840d941
Fabian_Eilber <fabian.eilber01@gmail.com>
2023-11-05 20:54:14 +0100

    [SCRUM 43] Kommunikation mit Eröffnungsdatenbank
    fix:
    - Chessosaurus.jar hat jackson.core dependency nicht eingebunden

--- src/main/java/chessosaurus/persistence/OpeninggameRestReader.java
+++ src/main/java/chessosaurus/persistence/OpeninggameRestReader.java
@@ -5,9 +5,6 @@ import chessosaurus.protocol.MoveParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
