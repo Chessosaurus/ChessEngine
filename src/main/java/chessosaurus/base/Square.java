6d2cdeb7a5940bbd4cd6d59907666943233bc0b9
Siedlerle <105000385+Siedlerle@users.noreply.github.com>
2023-12-01 11:13:55 +0100

    [SCRUM - 54] ANTLR Züge/Positionen parsen WIP

--- src/main/java/chessosaurus/base/Square.java
+++ src/main/java/chessosaurus/base/Square.java
@@ -110,4 +110,8 @@ public class Square {
     public boolean isEnPassantPossible() {
         return enPassantPossible;
     }
+
+    public boolean isField(char file, int rank){
+        return this.file == file && this.rank == rank;
+    }
 }
--- src/main/java/chessosaurus/parser/Converter.java
+++ src/main/java/chessosaurus/parser/Converter.java
@@ -28,38 +28,134 @@ public class Converter implements InputVisitor {
 
     @Override
     public Board visitMoves(InputParser.MovesContext ctx) {
-        Board b = new Board();
+        Board board = new Board();
         //Change to parse fen
-        b.importFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+        board.importFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
         //@TODO implement moves Syntax
         List<Move> moves = visitWhitemove(ctx.whitemove());
-        for(int i = 0; i< moves.size();i++){
+        for (int i = 0; i < moves.size(); i++) {
             Move move = moves.get(i);
-            //Rochade
-            if(i % 2 == 0 && move.getFrom())
-            //En passant
-
-            //Normal
-
-        }
-        for (Move m : moves) {
-            Piece p = b.getChessboard()
-                    [m.getFrom().getRank()]
-                    [m.getFrom().getFile()]
-                    .getPiece().get();
-            if(m.getPromoted().isPresent()){
-                p.setType(m.getPromoted().get());
+            if (move.getFrom().getPiece().isEmpty()) {
+                return null;
             }
-            b.getChessboard()
-                    [m.getFrom().getRank()]
-                    [m.getFrom().getFile()]
-                    .setPiece(Optional.empty());
-            b.getChessboard()
-                    [m.getTo().getRank()]
-                    [m.getTo().getFile()]
-                    .setPiece(p);
+            //White special turns
+            if (i % 2 == 0) {
+                //White-Castle
+                castle(board,move,1);
+//                if (move.getFrom().isField('e', 1) && move.getFrom().getPiece().get().getType() == PieceType.KING) {
+//                    if (move.getTo().isField('c', 1)) {
+//                        board = castleTo(board, 'c', 1);
+//                        //@TODO diable left white castle
+//                        continue;
+//                    }
+//                    if (move.getTo().isField('g', 1)) {
+//                        board = castleTo(board, 'g', 1);
+//                        //@TODO disable right white castle
+//                        continue;
+//                    }
+//                }
+                //White-En passant
+                enPass(board,move,5,6);
+//                if (move.getFrom().getFile() != move.getTo().getFile() //File is different
+//                        && move.getTo().getPiece().isEmpty()) //To Field is empty
+//                {
+//                    if (move.getFrom().getPiece().get().getType() == PieceType.PAWN //Pawn Move
+//                            && move.getFrom().getRank() == 5//Rank 5 to Rank 6
+//                            && move.getTo().getRank() == 6) {
+//                        board = enPassant(board, move, Color.WHITE);
+//                        continue;
+//                    }
+//                }
+            }
+            ;
+            //Black special turns
+            if (i % 2 == 1) {
+                //Black-Castle
+                castle(board,move,8);
+//                if (move.getFrom().isField('e', 8) && move.getFrom().getPiece().get().getType() == PieceType.KING) {
+//                    if (move.getTo().isField('c', 8)) {
+//                        board = castleTo(board, 'c', 8);
+//                        //@TODO diable left black castle
+//                        continue;
+//                    }
+//                    if (move.getTo().isField('g', 8)) {
+//                        board = castleTo(board, 'g', 8);
+//                        //@TODO disable right black castle
+//                        continue;
+//                    }
+//                }
+                //Black-En passant
+                enPass(board,move,4,3);
+//                if (move.getFrom().getFile() != move.getTo().getFile() //File is different
+//                        && move.getTo().getPiece().isEmpty()) //To Field is empty
+//                {
+//                    if (move.getFrom().getPiece().get().getType() == PieceType.PAWN //Pawn Move
+//                            && move.getFrom().getRank() == 4//Rank 4 to Rank 3
+//                            && move.getTo().getRank() == 3) {
+//                        board = enPassant(board, move, Color.BLACK);
+//                        continue;
+//                    }
+//                }
+            }
+            //Normal
+            move.getTo().setPiece(move.getFrom().getPiece());
+            move.getFrom().setPiece(Optional.empty());
+            //Promote
+            if(move.getPromoted().isPresent()){
+                move.getTo().getPiece().get().setType(move.getPromoted().get());
+            }
         }
-        return b;
+        return board;
+    }
+    private boolean castle(Board board, Move move,int rank){
+        if (move.getFrom().isField('e', rank) && move.getFrom().getPiece().get().getType() == PieceType.KING) {
+            if (move.getTo().isField('c', rank)) {
+                board = castleTo(board, 'c', rank);
+                //@TODO diable left white castle
+                return true;
+            }
+            if (move.getTo().isField('g', rank)) {
+                board = castleTo(board, 'g', rank);
+                //@TODO disable right white castle
+                return true;
+            }
+        }
+        return false;
+    }
+    private Board castleTo(Board board, char file, int rank) {
+        int fileInt = file == 'c' ? 2 : 6;
+        int rankInt = rank - 1;
+        //Move king to Square and reset King Square
+        board.getChessboard()[rankInt][fileInt].setPiece
+                (board.getChessboard()[rankInt][4].getPiece());
+        board.getChessboard()[rankInt][4].setPiece(Optional.empty());
+        //Move Rook behind King and reset Rook Square
+        board.getChessboard()[rankInt][fileInt == 2 ? 3 : 5].setPiece(
+                board.getChessboard()[rankInt][fileInt == 2 ? 0 : 7].getPiece());
+        board.getChessboard()[rankInt][fileInt == 2 ? 0 : 7].setPiece(Optional.empty());
+        return board;
+    }
+
+    private boolean enPass(Board board, Move move,int rankFrom, int rankTo){
+        if (move.getFrom().getFile() != move.getTo().getFile() //File is different
+                && move.getTo().getPiece().isEmpty()) //To Field is empty
+        {
+            if (move.getFrom().getPiece().get().getType() == PieceType.PAWN //Pawn Move
+                    && move.getFrom().getRank() == rankFrom//Rank 4 to Rank 3
+                    && move.getTo().getRank() == rankTo) {
+                board = enPassant(board, move, Color.BLACK);
+                return true;
+            }
+        }
+        return false;
+    }
+    private Board enPassant(Board board, Move move, Color color) {
+        int rankInt = move.getTo().getRank() - 1;
+        int fileInt = move.getTo().getFileVal() - 1;
+        move.getTo().setPiece(move.getFrom().getPiece());
+        move.getFrom().setPiece(Optional.empty());
+        board.getChessboard()[color == Color.WHITE ? rankInt - 1 : rankInt + 1][fileInt].setPiece(Optional.empty());
+        return board;
     }
 
     @Override
@@ -108,7 +204,7 @@ public class Converter implements InputVisitor {
     @Override
     public Board visitRows(InputParser.RowsContext ctx) {
         Board b = new Board();
-        for (int i = 8; i > 0; i--) {
+        for (int i = 7; i >= 0; i--) {
             b.setRank(i, visitSingle_row(ctx.row(i).single_row()));
         }
         return b;
@@ -123,12 +219,13 @@ public class Converter implements InputVisitor {
     public Optional<Piece>[] visitSingle_row(InputParser.Single_rowContext ctx) {
         Optional<Piece>[] rank = new Optional[8];
         //rank[0] = Optional.of(new Piece());
-        int index = 1;
+        int index = 0;
         for (InputParser.SlotContext sc : ctx.slot()) {
-            if (sc.piece().isEmpty()) {
+            if (sc.piece()==null) {
                 for (int i = 0; i < Integer.parseInt(sc.num().FILE().getText()); i++) {
                     rank[index++] = Optional.empty();
                 }
+                continue;
             }
             InputParser.Piece_allContext pc = sc.piece().piece_all();
             rank[index++] = Optional.of(visitPiece_all(pc));
@@ -183,7 +280,7 @@ public class Converter implements InputVisitor {
 
     @Override
     public Piece visitPiece_all(InputParser.Piece_allContext ctx) {
-        if (ctx.piece_white().isEmpty()) {
+        if (ctx.piece_white()==null) {
             return visitPiece_black(ctx.piece_black());
         } else {
             return visitPiece_white(ctx.piece_white());
@@ -192,21 +289,21 @@ public class Converter implements InputVisitor {
 
     @Override
     public Piece visitPiece_white(InputParser.Piece_whiteContext ctx) {
-        if (!ctx.king_white().isEmpty()) return new Piece(PieceType.KING, Color.WHITE);
-        if (!ctx.queen_white().isEmpty()) return new Piece(PieceType.QUEEN, Color.WHITE);
-        if (!ctx.rook_white().isEmpty()) return new Piece(PieceType.ROOK, Color.WHITE);
-        if (!ctx.knight_white().isEmpty()) return new Piece(PieceType.KNIGHT, Color.WHITE);
-        if (!ctx.bishop_white().isEmpty()) return new Piece(PieceType.BISHOP, Color.WHITE);
+        if (ctx.king_white() != null) return new Piece(PieceType.KING, Color.WHITE);
+        if (ctx.queen_white() != null) return new Piece(PieceType.QUEEN, Color.WHITE);
+        if (ctx.rook_white() != null) return new Piece(PieceType.ROOK, Color.WHITE);
+        if (ctx.knight_white() != null) return new Piece(PieceType.KNIGHT, Color.WHITE);
+        if (ctx.bishop_white() != null) return new Piece(PieceType.BISHOP, Color.WHITE);
         return new Piece(PieceType.PAWN, Color.WHITE);
     }
 
     @Override
     public Piece visitPiece_black(InputParser.Piece_blackContext ctx) {
-        if (!ctx.king_black().isEmpty()) return new Piece(PieceType.KING, Color.BLACK);
-        if (!ctx.queen_black().isEmpty()) return new Piece(PieceType.QUEEN, Color.BLACK);
-        if (!ctx.rook_black().isEmpty()) return new Piece(PieceType.ROOK, Color.BLACK);
-        if (!ctx.knight_black().isEmpty()) return new Piece(PieceType.KNIGHT, Color.BLACK);
-        if (!ctx.bishop_black().isEmpty()) return new Piece(PieceType.BISHOP, Color.BLACK);
+        if (ctx.king_black() != null) return new Piece(PieceType.KING, Color.BLACK);
+        if (ctx.queen_black() != null) return new Piece(PieceType.QUEEN, Color.BLACK);
+        if (ctx.rook_black() != null) return new Piece(PieceType.ROOK, Color.BLACK);
+        if (ctx.knight_black() != null) return new Piece(PieceType.KNIGHT, Color.BLACK);
+        if (ctx.bishop_black() != null) return new Piece(PieceType.BISHOP, Color.BLACK);
         return new Piece(PieceType.PAWN, Color.BLACK);
     }
 
